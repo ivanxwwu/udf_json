d830e8f0df4c8d5b401cb1fb42c529aedd5139f6
unboby <ivanlife55@163.com>
2018-05-20 11:57:24 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/udf_json.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udf_json.dir/main.cpp.o"
+  "udf_json.pdb"
+  "udf_json"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udf_json.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
